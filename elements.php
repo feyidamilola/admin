63925f7eb07fc9398904955b0c0eb3f5d0d48f96
Feyidamilola <pheyidamilola@gmail.com>
2019-03-11 11:35:37 +0100

    updated some feature

---
+++ elements.php
@@ -0,0 +1,40 @@
+<?php 
+  include 'header.php';
+?>
+        <div class="main-content">
+            <div class="h-20"></div>
+            <div class="col-md-12 mb-20">
+                <div>
+                    <ul class="breadcrumb pull-left">
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="all-users.php">Users</a>
+                        </li>
+                        <li>
+                            <a href="javascript:;" class="active">Add New User</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="">
+                <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                    <div class="page-content" style="overflow:hidden;">
+                        <div class="alert alert-success alert-dismissible">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            <strong>Success!</strong> Indicates a successful or positive action.
+                        </div>
+                        <div class="alert alert-danger alert-dismissible">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            <strong>Success!</strong> Indicates a successful or positive action.
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+<?php 
+    include 'footer.php';
+?>
